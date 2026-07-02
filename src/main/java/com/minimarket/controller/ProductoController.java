0a557b4f15266523083cda5aeca85a31091a5e5c
jorgesoto-13 <jorgesoto123789@gmail.com>
2026-07-01 23:47:27 -0500

    Update CrossOrigin to allow multiple origins

--- src/main/java/com/minimarket/controller/ProductoController.java
+++ src/main/java/com/minimarket/controller/ProductoController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/productos")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://minimarket-erp.vercel.app"})
 public class ProductoController {
 
     private final ProductoRepository productoRepository;
